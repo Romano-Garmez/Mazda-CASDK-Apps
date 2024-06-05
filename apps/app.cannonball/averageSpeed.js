8996a08b1beeb504b8ff27a8b5bc44ad1c5df317
Roman Garms <38168462+Romano-Garmez@users.noreply.github.com>
2024-06-05 15:44:10 -0700

    improved comments

--- apps/app.cannonball/averageSpeed.js
+++ apps/app.cannonball/averageSpeed.js
@@ -3,7 +3,11 @@ totalSpeed = 0;
 numOfSpeedData = 0;
 currentSpeed = -1;
 htmlElement = null;
+running = false;
 
+/* updateAverageSpeed
+ * Update the average speed with the current speed on HTML element
+ */
 function updateAverageSpeed(){
     if(avgSpeed == -1){
         avgSpeed = currentSpeed;
@@ -13,29 +17,50 @@ function updateAverageSpeed(){
         avgSpeed = totalSpeed / numOfSpeedData;
         numOfSpeedData++;
     }
-htmlElement.html(Math.round(getAverageSpeed() * 100) / 100);
+htmlElement.html(Math.round(avgSpeed * 100) / 100);
 
 return avgSpeed;
 }
 
+/* setCurrentSpeed
+ * Set the current speed to be used in the average speed calculation. Used to get speed from app.js
+ * inputCurrentSpeed: the current speed
+ */
 function setCurrentSpeed(inputCurrentSpeed){
     currentSpeed = inputCurrentSpeed;
 }
 
+/* startAvgSpeed
+ * Start the average speed calculation with an interval of 1 second
+ * inputHTMLElement: the HTML element to display the average speed
+ */
 function startAvgSpeed(inputHTMLElement){
+    running = true;
     htmlElement = inputHTMLElement;
     const interval = setInterval(function() {
         updateAverageSpeed()
       }, 1000);
-
-     
-     //clearInterval(interval); // thanks @Luca D'Amico
 }
 
-function getAverageSpeed(){
-    return avgSpeed;
+/* pauseCurrentSpeed
+ * Pause the average speed calculation interval
+ */
+function pauseCurrentSpeed(){
+    running = false;
+    clearInterval(interval);
 }
 
+/* resumeCurrentSpeed
+ * Resume the average speed calculation interval
+ */
+function resumeCurrentSpeed(){
+    running = true;
+    startAvgSpeed(htmlElement);
+}
+
+/* resetAverageSpeed
+ * Reset the average speed calculation and all data
+ */
 function resetAverageSpeed(){
     avgSpeed = -1;
     totalSpeed = 0;
--- apps/app.cannonball/stopwatch.js
+++ apps/app.cannonball/stopwatch.js
@@ -1,17 +1,27 @@
 startTime = -1;
 endTime = -1;
 swhtmlElement = null;
+running = false;
 
+/* startStopwatch
+ * Start the stopwatch with an interval of 1 second
+ * inputHTMLElement: the HTML element to display the stopwatch
+ */
 function startStopwatch(inputHTMLElement) {
     swhtmlElement = inputHTMLElement;
     startTime = new Date().getTime();
+    running = true;
 
-    const swinterval = setInterval(function() {
+    const swinterval = setInterval(function () {
         showStopwatchTime()
-      }, 1000);
+    }, 1000);
 }
 
+/* stopStopwatch
+ * Stop the stopwatch and return the time in milliseconds
+ */
 function stopStopwatch() {
+    running = false;
     if (startTime > 0) {
         endTime = new Date().getTime();
         startTime = -1;
@@ -20,12 +30,15 @@ function stopStopwatch() {
     return -1;
 }
 
+/* showStopwatchTime
+ * Show the time on the HTML element
+ */
 function showStopwatchTime() {
     if (startTime > 0) {
         duration = (new Date().getTime() - startTime);
         seconds = Math.floor((duration / 1000) % 60),
-        minutes = Math.floor((duration / (1000 * 60)) % 60),
-        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+            minutes = Math.floor((duration / (1000 * 60)) % 60),
+            hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
         hours = (hours < 10) ? "0" + hours : hours;
         minutes = (minutes < 10) ? "0" + minutes : minutes;
@@ -36,6 +49,25 @@ function showStopwatchTime() {
     return -1;
 }
 
+/* pauseStopwatch
+ * Pause the stopwatch interval
+ */
+function pauseStopwatch() {
+    running = false;
+    clearInterval(swinterval);
+}
+
+/* resumeStopwatch
+ * Resume the stopwatch interval
+ */
+function resumeStopwatch() {
+    running = true;
+    startStopwatch(swhtmlElement);
+}
+
+/* resetStopwatch
+ * Reset the stopwatch and all data
+ */
 function resetStopwatch() {
     startTime = -1;
     endTime = -1;
